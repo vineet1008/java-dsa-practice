c7419371553ada3849607776fa40e611c0cad1ba
Vineet <guptavineet287@gmail.com>
2026-07-24 16:08:40 +0530

    CountingNumbers logics added

--- src/com/vineet/basic/logics/CountingNumbers.java
+++ src/com/vineet/basic/logics/CountingNumbers.java
@@ -1,4 +1,17 @@
 package com.vineet.basic.logics;
 
 public class CountingNumbers {
+
+    public static void main(String[] args) {
+        System.out.println("Number of Count :: "+getCountOfNumbers(1234));
+    }
+
+    private static int getCountOfNumbers(int n){
+        int count=0;
+        while (n>0){
+            n=n/10;
+            count++;
+        }
+        return count;
+    }
 }
