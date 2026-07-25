498f6f960fa627577396fdaaa5f6c538e762ba41
Vineet <guptavineet287@gmail.com>
2026-07-25 21:37:40 +0530

    printsNumbers.java added

---
+++ src/com/logics/recursion/PrintsNumbers.java
@@ -0,0 +1,17 @@
+package com.logics.recursion;
+
+public class PrintsNumbers {
+
+    public static void main(String[] args) {
+        printNumbers(1);
+    }
+
+    private static void printNumbers(int n) {
+        if(n<=5){
+            return;
+        }
+        System.out.println(n);
+
+        printNumbers(n+1);
+    }
+}
