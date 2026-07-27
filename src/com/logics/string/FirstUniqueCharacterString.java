cda113a2e96f6ec56205297318066829a06e4a3f
Vineet <guptavineet287@gmail.com>
2026-07-27 18:19:48 +0530

    FirstUniqueCharacterString.java updated

--- src/com/logics/string/FirstUniqueCharacterString.java
+++ src/com/logics/string/FirstUniqueCharacterString.java
@@ -3,12 +3,13 @@ package com.logics.string;
 public class FirstUniqueCharacterString {
 
     public static void main(String[] args) {
-        String s="leetcode";
+        //String s="leetcode";
+        String s ="dddccdbba";
         System.out.println(firstUniqChar(s));
     }
 
     private static int firstUniqChar(String s) {
-        for (int i=0;i<s.length()-1;i++){
+        for (int i=0;i<s.length();i++){
             if(s.indexOf(s.charAt(i))==s.lastIndexOf(s.charAt(i))){
                 return i;
             }
