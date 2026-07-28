99536d7c6139885ebbb8edd0e9686e6495b461c2
Vineet <guptavineet287@gmail.com>
2026-07-28 18:43:33 +0530

    twoSum.java updated

--- src/com/logics/arrays/TwoSum.java
+++ src/com/logics/arrays/TwoSum.java
@@ -10,7 +10,7 @@ public class TwoSum {
     }
 
     private static int[] twoSum(int[] nums, int target) {
-        for (int i=0;i<nums.length;i++){
+        for (int i=0;i<nums.length;i++){ //using the brute force check every possible pair of numbers
            for(int j=i+1;j< nums.length;j++){
                 if(nums[i]+nums[j]==target){
                     return new int[]{i,j};
