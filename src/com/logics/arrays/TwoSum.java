7f590f63b19d2a4282c2906d64613c1965f3df85
Vineet <guptavineet287@gmail.com>
2026-07-28 16:34:59 +0530

    twoSum.java updated

--- src/com/logics/arrays/TwoSum.java
+++ src/com/logics/arrays/TwoSum.java
@@ -1,15 +1,23 @@
 package com.logics.arrays;
 
+import java.util.Arrays;
+
 public class TwoSum {
     public static void main(String[] args) {
         int[] nums={2,7,11,15};
-        int target=9;
-        System.out.println(twoSum(nums,target));
+        int target=17;
+        System.out.println(Arrays.toString(twoSum(nums,target)));
     }
 
     private static int[] twoSum(int[] nums, int target) {
-        int first=nums[0];
-
+        for (int i=0;i<nums.length;i++){
+           for(int j=i+1;j< nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+           }
+        }
+        return new int[]{};
 
     }
 }
