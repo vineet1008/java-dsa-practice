507119d3e5a7c55c311ded1ca9afbe0357a1693a
Vineet <guptavineet287@gmail.com>
2026-07-28 12:44:33 +0530

    MajorityElement.java updated

--- src/com/logics/numbers/MajorityElement.java
+++ src/com/logics/numbers/MajorityElement.java
@@ -7,21 +7,20 @@ public class MajorityElement {
 
     public static void main(String[] args) {
 //        int[] nums = {3,2,3};
-        int[] nums ={2,2,1,1,1,2,2};
+        int[] nums ={2,2,1,1,1,2,2}; //{2=4,1=3}
         System.out.println(majorityElement(nums));
     }
 
     private static int majorityElement(int[] nums) {
         int n=nums.length/2;
         Map<Integer,Integer> map=new HashMap<>();
-        int count=0;
+        int count=1;
         for (int i:nums){
             if(map.containsKey(i)){
-                map.put(i,count+1);
-            }else {
-                map.put(i,count);
+                map.put(i,map.get(i)+1);
+            }else{
+                map.put(i,1);
             }
-
         }
         System.out.println(map);
         for(Map.Entry<Integer,Integer> m:map.entrySet()){
